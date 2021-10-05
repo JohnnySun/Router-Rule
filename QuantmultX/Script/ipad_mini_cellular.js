dedfeaedf67bbbda7cb968308e5f3fabcaacaad1
JohnnySun <bmy001@gmail.com>
2021-10-05 11:17:03 +0000

    update ipad_mini_cellular.js

--- QuantmultX/Script/ipad_mini_cellular.js
+++ QuantmultX/Script/ipad_mini_cellular.js
@@ -1,5 +1,5 @@
- const url = "https://www.apple.com.cn/shop/buy-ipad/ipad-pro";
- const method = "POST";
+ const url = "https://www.apple.com.cn/shop/buy-ipad/ipad-mini";
+ const method = "GET";
  
  const myRequest = {
      url: url,
@@ -9,13 +9,13 @@
  $task.fetch(myRequest).then(response => {
      // response.statusCode, response.headers, response.body
      const word = '无线局域网 + 蜂窝网络机型将在获得批准后发售';
-     var has_released = !response.body.include(word);
+     var has_released = !response.body.includes(word);
      console.log(response.body);
-     $notify("iPad Mini 发售状态", "还未发售", "还未发售"); 
+     $notify("iPad Mini 蜂窝版发售状态", "蜂窝版还未发售", "蜂窝版还未发售"); 
      $done();
  }, reason => {
      // reason.error
-     $notify("iPad Mini 发售状态", "网络请求失败", "网络请求失败"); 
+     $notify("iPad Mini 蜂窝版发售状态", "网络请求失败", "网络请求失败"); 
      $done();
  });
  
