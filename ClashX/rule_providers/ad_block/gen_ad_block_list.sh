3e09d79ad0042af6a0709dcd2a29929272bb05cc
Johnny Sun <sunhang.io@bytedance.com>
2023-10-24 19:24:17 +0800

    bugfix

--- ClashX/rule_providers/ad_block/gen_ad_block_list.sh
+++ ClashX/rule_providers/ad_block/gen_ad_block_list.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-payload=`curl -L https://raw.githubusercontent.com/dler-io/Rules/main/Clash/Provider/Reject.yaml`
+url=" https://raw.githubusercontent.com/dler-io/Rules/main/Clash/Provider/Reject.yaml"
+
+payload=$(curl -s "$url")
 
 echo "payload:" > $1/ad_block_domain.yaml
-echo -n $payload | grep DOMAIN-SUFFIX >> $1/ad_block_domain.yaml
+printf $payload | grep DOMAIN-SUFFIX >> $1/ad_block_domain.yaml
 
 echo "payload:" > $1/ad_block_ipcidr.yaml
-echo -n $payload | grep IP-CIDR > $1/ad_block_ipcidr.yaml
+printf $payload | grep IP-CIDR > $1/ad_block_ipcidr.yaml
