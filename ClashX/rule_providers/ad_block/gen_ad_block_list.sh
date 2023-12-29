9de3c61249610298700503029a68309a9b0fcfb5
JohnnySun <bmy001@gmail.com>
2023-12-29 14:08:07 +0800

    fix ad_block_domain

--- ClashX/rule_providers/ad_block/gen_ad_block_list.sh
+++ ClashX/rule_providers/ad_block/gen_ad_block_list.sh
@@ -8,6 +8,8 @@ echo "payload:" > $1/ad_block_domain.yaml
 printf "$payload" | grep DOMAIN >> $1/ad_block_domain.yaml
 # parse to +.domain.com
 sed -i "s/- DOMAIN\(.*\),\(.*\)/- '+.\2'/g" "$1/ad_block_domain.yaml"
+
+printf "$payload" | grep DOMAIN >> $1/ad_block_domain.txt
 sed -i "s/- DOMAIN-SUFFIX,/./g" "$1/ad_block_domain.txt"
 sed -i "s/- DOMAIN,//g" "$1/ad_block_domain.txt"
 sed '/^ \+/d' "$1/ad_block_domain.txt"
