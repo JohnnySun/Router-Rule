46a6fb4db47a04432651fcc0e8cfcc8f1b538a9f
JohnnySun <bmy001@gmail.com>
2023-12-29 14:05:50 +0800

    new ad_block_domain txt support

--- ClashX/rule_providers/ad_block/gen_ad_block_list.sh
+++ ClashX/rule_providers/ad_block/gen_ad_block_list.sh
@@ -8,6 +8,9 @@ echo "payload:" > $1/ad_block_domain.yaml
 printf "$payload" | grep DOMAIN >> $1/ad_block_domain.yaml
 # parse to +.domain.com
 sed -i "s/- DOMAIN\(.*\),\(.*\)/- '+.\2'/g" "$1/ad_block_domain.yaml"
+sed -i "s/- DOMAIN-SUFFIX,/./g" "$1/ad_block_domain.txt"
+sed -i "s/- DOMAIN,//g" "$1/ad_block_domain.txt"
+sed '/^ \+/d' "$1/ad_block_domain.txt"
 
 echo "payload:" > $1/ad_block_ipcidr.yaml
 printf "$payload" | grep IP-CIDR | sed 's/IP-CIDR.*,//g'>> $1/ad_block_ipcidr.yaml
