1c1ad04e3997d07f711caf67134805475a97aa97
JohnnySun <bmy001@gmail.com>
2023-12-29 14:39:17 +0800

    change name

--- ClashX/rule_providers/ad_block/gen_ad_block_list.sh
+++ ClashX/rule_providers/ad_block/gen_ad_block_list.sh
@@ -9,10 +9,10 @@ printf "$payload" | grep DOMAIN >> $1/ad_block_domain.yaml
 # parse to +.domain.com
 sed -i "s/- DOMAIN\(.*\),\(.*\)/- '+.\2'/g" "$1/ad_block_domain.yaml"
 
-printf "$payload" | grep DOMAIN >> $1/ad_block_domain.txt
-sed -i "s/- DOMAIN-SUFFIX,/./g" "$1/ad_block_domain.txt"
-sed -i "s/- DOMAIN,//g" "$1/ad_block_domain.txt"
-sed -i "s/^ \+//g" "$1/ad_block_domain.txt"
+printf "$payload" | grep DOMAIN >> $1/ad_block_domain.text
+sed -i "s/- DOMAIN-SUFFIX,/./g" "$1/ad_block_domain.text"
+sed -i "s/- DOMAIN,//g" "$1/ad_block_domain.text"
+sed -i "s/^ \+//g" "$1/ad_block_domain.text"
 
 echo "payload:" > $1/ad_block_ipcidr.yaml
 printf "$payload" | grep IP-CIDR | sed 's/IP-CIDR.*,//g'>> $1/ad_block_ipcidr.yaml
